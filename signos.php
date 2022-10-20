5d2db6d22480e3fe7e7628e2d0c1d2077031cbe0
mssfrc1 <116230612+mssfrc1@users.noreply.github.com>
2022-10-20 00:47:53 -0300

    Add files via upload

---
+++ signos.php
@@ -0,0 +1,38 @@
+<?php
+
+$xml = simplexml_load_file('signos.xml');
+$nome = $_GET['nome'];
+$nome = utf8_decode($nome);
+$inputdata = $_GET['inputdata'];
+$calculosigno = explode('-',$inputdata);
+
+foreach ($calculosigno as $stringarray){
+    $dia = (int)$calculosigno[2];
+    $mes = (int)$calculosigno[1];
+    $ano = (int)$calculosigno[0];
+};
+
+$dia2 = $dia.'/'.$mes.'/'.$ano;
+$join = $mes.'/'.$dia;
+$join = strtotime($join);
+//echo '<p>'.'O valor do join é '.$join.'</p>';
+
+//(int)$join = $dia.$mes;
+//var_dump ($join);
+
+foreach($xml->signo as $signos){
+    $dataIni = explode('/', $signos->dataInicio);
+    $dataIni = $dataIni[1].'/'.$dataIni[0];
+
+    $dataFim = explode('/', $signos->dataFim);
+    $dataFim = $dataFim[1].'/'.$dataFim[0];
+
+    //echo '<p>'.var_dump($dataIni).var_dump($dataFim).var_dump(strtotime($dataIni)).' '.var_dump(strtotime($dataFim)).'</p>';
+      if($join <= strtotime($dataFim) && $join >= strtotime($dataIni)){
+            echo 'Olá, '.$nome.' o seu aniversário é no dia: '.$dia2;
+            echo '<p>'.'O seu signo é: '.$signos->signoNome.'</p>';
+            echo '<p>'.'As características desse signo são: '.$signos->descricao.'</p>';
+        }    
+}
+
+?>
